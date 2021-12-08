8ec2d58b3fa3d888642d87b6b3e47a60bb5db7c2
Pedro Ramos <medpsr9@gmail.com>
2021-12-07 18:52:26 -0600

    lol

--- Phase-1-LightSaberQuiz/index.js
+++ Phase-1-LightSaberQuiz/index.js
@@ -3,3 +3,29 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Hi")
 })
+
+// $(document).ready(function(){ 
+ 
+//     $(".primaryContained").on('click', function(){
+//     $(".comment").addClass("commentClicked");
+//   });//end click
+//   $("textarea").on('keyup.enter', function(){
+//     $(".comment").addClass("commentClicked");
+//   });//end keyup
+//   });//End Function
+ 
+// new Vue({
+//     el: "#app",
+//     data:{
+//        title: 'Add a comment',
+//       newItem: '',
+//       item: [],
+//     },
+//     methods:{
+//       addItem  (){
+//       this.item.push(this.newItem);
+//         this.newItem = "";
+//       }
+//   }
+ 
+//   });
